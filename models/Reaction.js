8eb1d2163b79cde14a601588439ebd21be906cd2
YimmyC <jim.cassano1@gmail.com>
2023-03-09 08:07:41 -0600

    started models

---
+++ models/Reaction.js
@@ -0,0 +1,19 @@
+const { Schema } = require("mongoose");
+// import dateFormat from "../utils/dateFormat";
+
+const reactionSchema = new Schema(
+  {
+    reactionId: { type: Schema.Types.ObjectId, default: new Types.ObjectId() },
+    reactionBody: { type: String, required: true, maxlength: 280 },
+    username: [{ type: String, required: true }],
+    createdAt: [{ type: Date, default: Date.now, get: (timestamp) => dateFormat(timestamp) }],
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+module.exports = reactionSchema;
