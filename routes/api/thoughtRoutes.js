2d32999d152c30b2c3840478d25ccb8dc81450fa
YimmyC <jim.cassano1@gmail.com>
2023-03-23 10:00:45 -0500

    create a thought working

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,8 +1,10 @@
 const router = require("express").Router();
-const { getSingleThought, getThoughts, createThought } = require("../../controllers/thoughtController");
+const { getSingleThought, getThoughts, createThought, updateThought, deleteThought } = require("../../controllers/thoughtController");
 
+// api/thoughts
 router.route("/").get(getThoughts).post(createThought);
 
-router.route("/:postId").get(getSingleThought);
+// api/thoughts/:thoughtId
+router.route("/:thoughtId").get(getSingleThought).put(updateThought).delete(deleteThought);
 
 module.exports = router;
